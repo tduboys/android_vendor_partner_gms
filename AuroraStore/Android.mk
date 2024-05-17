49be50eb34c90f592881589adbbcc397e36aa1c1
Thomas du Boÿs <thomas@duboys.info>
2024-05-17 14:25:56 +0200

    Keep Aurora Services signature and add optional libs for Android 14 builds

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -14,7 +14,11 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.aurora.store.xml
-LOCAL_CERTIFICATE := releasekey
+LOCAL_CERTIFICATE := PRESIGNED
+# these lines may break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
