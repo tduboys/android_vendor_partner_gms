c3af4218337dda18683fc5b21bd2f02211969744
Thomas du Boÿs <thomas@duboys.info>
2024-05-18 12:29:18 +0200

    Add optional libs on AndroidAuto manifest

--- AndroidAuto/Android.mk
+++ AndroidAuto/Android.mk
@@ -15,6 +15,10 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := com.google.android.projection.gearhead.xml
+# these lines may break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
