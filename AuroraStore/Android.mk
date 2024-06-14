008f82ed6f45edd3c974e45fcfbac6e332f71c99
Thomas du Boÿs <thomas@duboys.info>
2024-06-14 10:48:20 +0200

    Update Aurora Store to 4.5.0 and install as non-privileged app

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -1,19 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-com.aurora.store.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := AuroraStore
 LOCAL_SRC_FILES := AuroraStore.apk
 LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.aurora.store.xml
 LOCAL_CERTIFICATE := PRESIGNED
 # these lines may break builds before 19.1 so make them conditional
 ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
